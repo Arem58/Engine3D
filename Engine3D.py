4d785c3721c1cc430a7c22bc41a2be3cb8dc4b01
churly92 <churly92@gmail.com>
2021-07-12 20:56:49 -0600

    Lineas

--- Engine3D.py
+++ Engine3D.py
@@ -1,20 +1,27 @@
 # Programa principal
-from gl import Renderer
+from gl import Renderer, V2, color
+from numpy import sin, cos
+
 
 width = 960
 height = 540
 
 rend = Renderer(width, height)
 
-rend.glClearColor(0, 0.3, 0)
-rend.glClear()
+#rend.glViewport
+#rend.glLine(V2(2, 5), V2(900, 420), color(1,0,0))
+#rend.glLine(V2(0, 0), V2(40, 500), color(0,1,0))
+#rend.glLine(V2(900, 500), V2(600, 100), color(0,0,1))
+#rend.glLine(V2(200, 400), V2( 500, 400), color(0.2,0.2,0.2))
 
-rend.glColor(0.2, 1, 1)
+for x in range(width):
+    x0 = x 
+    x1 = x + 1
 
-rend.glPoint(700, 350)
+    y0 = sin(x0 * 0.1) * 50 + height/2
+    y1 = sin(x1 * 0.1) * 50 + height/2
 
-for i in range(200):
-    rend.glPoint(i,i)
+    rend.glLine(V2(int(x0), int(y0)), V2(int(x1), int(y1)))
 
 rend.glFinish("output.bmp")
 
--- gl.py
+++ gl.py
@@ -1,4 +1,8 @@
 import struct
+from collections import namedtuple
+
+V2 = namedtuple('Point2', ['x', 'y'])
+
 
 def char(c):
     # 1 byte
@@ -33,6 +37,15 @@ class Renderer(object):
         self.width = width
         self.height = height
         self.glClear()
+        self.glViewport(0,0, width, height)
+
+    def glViewport(self, x, y, width, height):
+        self.vpX = x
+        self.vpY = y
+        self.vpWidth = width
+        self.vpHeight = height
+
+
 
     def glClearColor(self, r, g, b):
         self.clear_color = color(r, g, b)
@@ -44,8 +57,51 @@ class Renderer(object):
     def glColor(self, r, g, b):
         self.curr_color = color(r,g,b)
 
-    def glPoint(self, x, y):
-        self.pixels[x][y] = self.curr_color
+
+    def glPoint(self, x, y, color = None):
+        if x < self.vpX or x >= self.vpX + self.vpWidth or y < self.vpY or y >= self.vpY + self.vpHeight:
+            return
+
+        if (0 < x < self.width) and (0 < y < self.height):
+            self.pixels[int(x)][int(y)] = color or self.curr_color
+
+    def glLine(self, v0, v1, color = None):
+        x0 = v0.x
+        x1 = v1.x
+        y0 = v0.y
+        y1 = v1.y
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        steep = dy > dx
+
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        offset = 0
+        limit = 0.5
+        m = dy/dx
+        y = y0
+
+        for x in range(x0, x1 + 1):
+            if steep:
+                self.glPoint(y, x, color)
+            else:
+                self.glPoint(x, y, color)
+
+            offset += m
+            if offset >= limit:
+                y += 1 if y0 < y1 else -1
+                limit += 1
 
     def glFinish(self, filename):
         #Crea un archivo BMP y lo llena con la información dentro de self.pixels
